0660ad1e4b900dc9b1c878eb59907f6fc0db5051
Ray Xu <38930179+buluoray@users.noreply.github.com>
2021-09-17 14:23:55 -1000

    update version
    update version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AnyImageKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "AnyImageKit", targets: ["AnyImageKit"]),
     ],
